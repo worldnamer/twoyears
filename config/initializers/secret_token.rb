6ffdabff2ed03a598e9d34a8e7eda4cf32e8e41c
John LeBoeuf-Little <worldnamer@worldnamer.com>
2013-11-13 01:17:48 -0700

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twoyears::Application.config.secret_token = '398e55a666defa90716ea8f2e79700428f40ec1b89801a6c2d9548eb0159524cf6774b340fd8c96068b84bd862e212452209489870e58907bdfb45b17c11d733'
