a9d259d5a6c1bb50d5b22dd7740931aae9976f58
John LeBoeuf-Little <worldnamer@worldnamer.com>
2013-11-13 02:35:59 -0700

    Add seeds file. Fix CommitFileParser bugs.

--- lib/commit_file_parser.rb
+++ lib/commit_file_parser.rb
@@ -2,8 +2,14 @@ class CommitFileParser
   def parse(filename)
     File.readlines(filename).each do |line|
       commit_hash = line[0,7]
-      committed_at = Time.parse(line[9, 37])
-      message = line[39, line.length-40]
+      
+      message_starts_at = line.index('0700')+4+1 if line.index('0700')
+      message_starts_at = line.index('0600')+4+1 if line.index('0600')
+      message_starts_at = line.index('0000')+4+1 if line.index('0000') and not message_starts_at
+
+      committed_at = Time.parse(line[8,message_starts_at-8-1])
+
+      message = line[message_starts_at, line.length-message_starts_at-1]
       Commit.create(commit_hash: commit_hash, committed_at: committed_at, message: message)
     end
   end
--- spec/lib/commit_file_parser_spec.rb
+++ spec/lib/commit_file_parser_spec.rb
@@ -11,6 +11,6 @@ describe CommitFileParser do
     commit = Commit.where(commit_hash: "92e643c").first
     commit.should be_present
     commit.message.should == 'Fix for ticket 3460, clicking menu items on user page is fidgety. Removing :active on button_base so that opacity will stay at 100% for click events. This is required because for whatever reason, Chrome will send click events to background elements with higher opacity.'
-    commit.committed_at.should == Time.parse('Tue Feb 21 17:54:31 2012 -0700')
+    commit.committed_at.should == Time.parse('Tue Feb 1 17:54:31 2012 -0700')
   end
 end
