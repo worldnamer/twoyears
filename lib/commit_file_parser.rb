fc9f96ac334c0e3e9d70676c6a24f79cc7ddf25b
John LeBoeuf-Little <worldnamer@worldnamer.com>
2013-11-16 15:38:01 -0700

    Convert file parser to use the same CSV format we export.

--- lib/commit_file_parser.rb
+++ lib/commit_file_parser.rb
@@ -1,17 +1,17 @@
+require 'csv'
+
 class CommitFileParser
   def parse(filename)
-    File.readlines(filename).each do |line|
-      hash_start = line.index(' ')+1
-      commit_hash = line[0,hash_start-1]
+    CSV.foreach(filename, headers: true) do |row|
+      commit_hash = row[0]
+      committed_at = row[1]
+      message = row[2]
+      tags = row[3].split(",")
 
-      message_starts_at = line.index('0700')+4+1 if line.index('0700')
-      message_starts_at = line.index('0600')+4+1 if line.index('0600')
-      message_starts_at = line.index('0000')+4+1 if line.index('0000') and not message_starts_at
-
-      committed_at = DateTime.strptime(line[hash_start,message_starts_at-hash_start-1], '%a %b %d %H:%M:%S %Y %z')
-
-      message = line[message_starts_at, line.length-message_starts_at-1]
-      Commit.create(commit_hash: commit_hash, committed_at: committed_at, message: message)
+      commit = Commit.create(commit_hash: commit_hash, committed_at: committed_at, message: message)
+      tags.each do |tag|
+        commit.tags << Tag.create(text: tag)
+      end
     end
   end
 end
--- spec/lib/commit_file_parser_spec.rb
+++ spec/lib/commit_file_parser_spec.rb
@@ -11,7 +11,7 @@ describe CommitFileParser do
     commit = Commit.where(commit_hash: "92e643c").first
     commit.should be_present
     commit.message.should == 'Fix for ticket 3460, clicking menu items on user page is fidgety. Removing :active on button_base so that opacity will stay at 100% for click events. This is required because for whatever reason, Chrome will send click events to background elements with higher opacity.'
-    commit.committed_at.should == DateTime.strptime('Tue Feb 1 17:54:31 2012 -0700', '%a %b %d %H:%M:%S %Y %z')
+    commit.committed_at.should == DateTime.strptime('Tue Feb 1 00:54:31 2012 UTC', '%a %b %d %H:%M:%S %Y %z')
   end
 
   it 'handles GMT dates' do
@@ -21,6 +21,18 @@ describe CommitFileParser do
 
     commit = Commit.where(commit_hash: "7b1f786").first
     commit.should be_present
-    commit.committed_at.should == DateTime.strptime('Tue Feb 21 14:15:10 2012 +0000', '%a %b %d %H:%M:%S %Y %z')
+    commit.committed_at.should == DateTime.strptime('Tue Feb 21 21:15:10 2012 +0000', '%a %b %d %H:%M:%S %Y %z')
+  end
+
+  it 'creates tags' do
+    parser = CommitFileParser.new
+
+    parser.parse("spec/lib/commit_file_parser/success.txt")
+
+    commit = Commit.where(commit_hash: "8706c9a").first
+    commit.should be_present
+    commit.tags.count.should == 2
+    commit.tags.first.text.should == "ui"
+    commit.tags.last.text.should == "super fly"
   end
 end
