999cfebe5648cfbcbbe1969dab0065c17ae74fd6
John LeBoeuf-Little <worldnamer@worldnamer.com>
2013-11-16 16:44:58 -0700

    Replacing plaintext seed file with ciphered seed file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-CommitFileParser.new.parse('db/mine_log.txt')
+puts "seeding database"
+CommitFileParser.new.parse('db/commits.csv')
+puts "finished"
