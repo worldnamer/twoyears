a109017f8c86ed97b68c3f81246ac976edcecca6
John LeBoeuf-Little <worldnamer@worldnamer.com>
2013-11-16 12:34:16 -0700

    Performance improvement for counts_by_day.

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -16,10 +16,10 @@ class Tag < ActiveRecord::Base
     # tags_per_day = select("date(committed_at), text, count(1) as tag_count").group("date(committed_at)").group("text").sort_by(&:committed_at)
 
     # get the earliest date
-    earliest_date = Commit.order("committed_at asc").first.committed_at
+    earliest_date = Commit.select("committed_at").order("committed_at asc").first.committed_at
 
     # get the latest date
-    latest_date = Commit.order("committed_at desc").first.committed_at
+    latest_date = Commit.select("committed_at").order("committed_at desc").first.committed_at
 
     # create a blank array for those dates
     blank_data = []
@@ -28,17 +28,17 @@ class Tag < ActiveRecord::Base
     end
 
     series = {} # "tag" => [{x: date_as_integer, y: count_on_day}, ...]
-    Commit.all.each do |commit|
+    Commit.includes(:tags).each do |commit|
       # find out what day the commit came in on, as an int
       committed_at = commit.committed_at.to_date.to_time.to_i
 
       # for each tag per commit
       commit.tags.each do |tag|
         #  create the blank array for that tag, if needed
-        series[tag.text] ||= Marshal.load(Marshal.dump(blank_data))
+        series[tag.text.strip] ||= Marshal.load(Marshal.dump(blank_data))
 
         #  fill in this day in that array
-        tuple_for_date = series[tag.text].detect { |tuple| tuple[:x] == committed_at }
+        tuple_for_date = series[tag.text.strip].detect { |tuple| tuple[:x] == committed_at }
         tuple_for_date[:y] ||= 0
         tuple_for_date[:y] += 1
       end
