ccb5c6645d18d68c13c80c6c321dd01819853880
John LeBoeuf-Little <worldnamer@worldnamer.com>
2013-11-18 17:09:23 -0700

    Patch for bug in parser where tags would get duplicated during seeding.

--- lib/commit_file_parser.rb
+++ lib/commit_file_parser.rb
@@ -9,8 +9,14 @@ class CommitFileParser
       tags = row[3].split(",")
 
       commit = Commit.create(commit_hash: commit_hash, committed_at: committed_at, message: message)
-      tags.each do |tag|
-        commit.tags << Tag.create(text: tag)
+      tags.each do |tag_text|
+        tag = Tag.where(text:tag_text).first
+        if tag
+          commit.tags << tag
+        else
+          commit.tags << Tag.create(text: tag_text)
+        end
+        commit.save
       end
     end
   end
